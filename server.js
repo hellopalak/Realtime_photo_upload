de7e04a23a8bc87eb5b861eb4d59b427e8f73c7c
advancedlooser70-hue <advancedlooser70@gmail.com>
2026-09-10 00:48:47 +0530

    allowed the input of videos also

--- server.js
+++ server.js
@@ -31,6 +31,10 @@ app.set('io', io);
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use((req, res, next) => {
+  console.log(`➡️ ${req.method} ${req.url}`);
+  next();
+});
 
 // Serve uploaded files statically
 app.use('/uploads', express.static(path.join(__dirname, 'public', 'uploads'), {
